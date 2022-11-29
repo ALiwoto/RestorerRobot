6885eedd79ba566093c0c5230a4fa63060f338d2
Aliwoto <aminnimaj@gmail.com>
2022-11-29 22:14:37 +0330

    Add some comments to fields of BackupScheduleContainer and BackupScheduleManager types.
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

--- src/wotoActions/backupPlugin/types.go
+++ src/wotoActions/backupPlugin/types.go
@@ -9,17 +9,40 @@ import (
 )
 
 type BackupScheduleContainer struct {
+	// DatabaseConfig is the config section of the target db belonging to
+	// this container.
 	DatabaseConfig *wotoConfig.ValueSection
+
+	// LastBackupDate is the date of the last time we got backup from this db.
 	LastBackupDate time.Time
+
+	// BackupInterval is the interval that we will get backup.
 	BackupInterval time.Duration
-	currentInfo    *wotoGlobals.BackupInfo
-	ChatIDs        []int64
-	mut            *sync.Mutex
-	isSleeping     bool
+
+	// currentInfo fields contains the current BackupInfo of this container.
+	currentInfo *wotoGlobals.BackupInfo
+
+	// ChatIDs field specifies the global log channels that we have
+	// to send the backup to.
+	ChatIDs []int64
+
+	// mut is a shared mutex between all related BackupScheduleContainer values
+	// (normally, all values, unless mentioned otherwise.)
+	mut *sync.Mutex
+
+	// isSleeping fields specifies if the current container is in sleeping phase.
+	isSleeping bool
 }
 
 type BackupScheduleManager struct {
-	containers    []*BackupScheduleContainer
+	// containers is an array of all existing BackupScheduleContainer that
+	// needs to be checked and handled by this BackupScheduleManager value.
+	containers []*BackupScheduleContainer
+
+	// checkInterval is the interval of each for loop of checker.
 	checkInterval time.Duration
-	ChatIDs       []int64
+
+	// ChatIDs field specifies the global log channels that we have
+	// to send the backup to.
+	ChatIDs []int64
 }
